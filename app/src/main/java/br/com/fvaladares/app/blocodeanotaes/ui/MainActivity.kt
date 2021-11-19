a2b77bd2b17d6fd89694f4c14e9ebb80056a0d37
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-11-19 17:45:03 -0300

    First commit

---
+++ app/src/main/java/br/com/fvaladares/app/blocodeanotaes/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fvaladares.app.blocodeanotaes.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.fvaladares.app.blocodeanotaes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
