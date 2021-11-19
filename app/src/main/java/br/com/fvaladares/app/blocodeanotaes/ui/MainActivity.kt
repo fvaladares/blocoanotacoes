2903f19645acc8cd9c0761bb5c8ff650be3f2ba9
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-11-19 19:07:12 -0300

    Criação do layout básico do app

--- app/src/main/java/br/com/fvaladares/app/blocodeanotaes/ui/MainActivity.kt
+++ app/src/main/java/br/com/fvaladares/app/blocodeanotaes/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.fvaladares.app.blocodeanotaes.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import br.com.fvaladares.app.blocodeanotaes.R
+import br.com.fvaladares.app.blocodeanotaes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
